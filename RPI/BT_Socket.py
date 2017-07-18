f27a196af950d2eabadbfe9bf8c26c7d14396729
Sebastian Gürtl <sebastian.guertl@gmail.com>
2017-07-18 17:53:30 +0200

    Add bluetooth socket

---
+++ RPI/BT_Socket.py
@@ -0,0 +1,43 @@
+import struct
+from bluetooth import *
+
+server_sock = BluetoothSocket(RFCOMM)
+server_sock.bind("", PORT_ANY)
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service(server_sock, "DronePi", service_classes = [ uuid, SERIAL_PORT_CLASS ], profiles = [SERIAL_PORT_PROFILE ])
+
+while True:
+    print('Waiting for connection on RFCOMM channel %d' % port)
+
+    client_sock, client_info = server_sock.accept()
+    print('Accepted connection from ', client_info)
+
+    while True:
+        data = client_sock.recv(19)
+
+        for byte in data:
+            byte = ord(byte)
+            byte = bin(byte)[2:].rjust(8, '0')
+            bits = ''
+            for bit in byte:
+                bits = bit + bits
+
+    try:
+        data = client_sock.recv(19)
+        print(data)
+    except IOError:
+        print('IOError')
+
+    except KeyboardInterrupt:
+        print('Disconnected')
+
+        client_sock.close()
+        server_sock.close()
+        print('All done')
+
+        break
