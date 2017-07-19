9e83d2e9e97577c466d6061bbab5cb830f4866c8
Roflmao100 <klaadb13@htl-kaindorf.ac.at>
2017-07-19 12:51:55 +0200

    Add Controller activity

---
+++ Dronection/Android/WiFi/WiFiDronection/WiFiDronection/ControllerSettings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WiFiDronection
+{
+    public class ControllerSettings
+    {
+        public static readonly bool ACTIVE = true;
+        public static readonly bool INACTIVE = false;
+
+        public bool Inverted
+        {
+            get;
+            set;
+        }
+
+        public int TrimYaw
+        {
+            get;
+            set;
+        }
+
+        public int TrimPitch
+        {
+            get;
+            set;
+        }
+
+        public int TrimRoll
+        {
+            get;
+            set;
+        }
+
+        public bool HeightControlActivated
+        {
+            get;
+            set;
+        }
+
+        public ControllerSettings()
+        {
+
+        }
+    }
+}
--- Dronection/Android/WiFi/WiFiDronection/WiFiDronection/Resources/Resource.Designer.cs
+++ Dronection/Android/WiFi/WiFiDronection/WiFiDronection/Resources/Resource.Designer.cs
@@ -47,6 +47,9 @@ namespace WiFiDronection
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int MySeekBar = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -60,20 +63,53 @@ namespace WiFiDronection
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050008
+			public const int TvRollTrim = 2131034120;
+			
+			// aapt resource value: 0x7f05000d
+			public const int btSendData = 2131034125;
+			
+			// aapt resource value: 0x7f05000b
+			public const int btShowLog = 2131034123;
+			
+			// aapt resource value: 0x7f05000a
+			public const int btStart = 2131034122;
+			
+			// aapt resource value: 0x7f05000f
+			public const int etDialogPassword = 2131034127;
+			
+			// aapt resource value: 0x7f05000c
+			public const int etInput = 2131034124;
+			
+			// aapt resource value: 0x7f05000e
+			public const int lvPeers = 2131034126;
+			
 			// aapt resource value: 0x7f050001
-			public const int btSendData = 2131034113;
-			
-			// aapt resource value: 0x7f050004
-			public const int etDialogPassword = 2131034116;
-			
-			// aapt resource value: 0x7f050000
-			public const int etInput = 2131034112;
-			
-			// aapt resource value: 0x7f050003
-			public const int lvPeers = 2131034115;
+			public const int rbThrottleLeft = 2131034113;
 			
 			// aapt resource value: 0x7f050002
-			public const int tvHeader = 2131034114;
+			public const int rbThrottleRight = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int rgControlMethod = 2131034112;
+			
+			// aapt resource value: 0x7f050007
+			public const int sbPitchTrim = 2131034119;
+			
+			// aapt resource value: 0x7f050009
+			public const int sbRollTrim = 2131034121;
+			
+			// aapt resource value: 0x7f050005
+			public const int sbYawTrim = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int tvDescription = 2131034115;
+			
+			// aapt resource value: 0x7f050006
+			public const int tvPitchTrim = 2131034118;
+			
+			// aapt resource value: 0x7f050004
+			public const int tvYawTrim = 2131034116;
 			
 			static Id()
 			{
@@ -89,13 +125,16 @@ namespace WiFiDronection
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int DataTransfer = 2130903040;
+			public const int ControllerSettings = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int DataTransfer = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int WifiDialog = 2130903042;
+			public const int Main = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int WifiDialog = 2130903043;
 			
 			static Layout()
 			{
