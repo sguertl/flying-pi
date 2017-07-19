091c9d8f396b5f76d09c8dfd921cefd826ded002
Sebastian Gürtl <sebastian.guertl@gmail.com>
2017-07-19 12:43:48 +0200

    Update MainActivity layout

--- Dronection/Android/WiFi/WiFiDronection/WiFiDronection/Resources/Resource.Designer.cs
+++ Dronection/Android/WiFi/WiFiDronection/WiFiDronection/Resources/Resource.Designer.cs
@@ -63,14 +63,17 @@ namespace WiFiDronection
 			// aapt resource value: 0x7f050001
 			public const int btSendData = 2131034113;
 			
-			// aapt resource value: 0x7f050003
-			public const int etDialogPassword = 2131034115;
+			// aapt resource value: 0x7f050004
+			public const int etDialogPassword = 2131034116;
 			
 			// aapt resource value: 0x7f050000
 			public const int etInput = 2131034112;
 			
+			// aapt resource value: 0x7f050003
+			public const int lvPeers = 2131034115;
+			
 			// aapt resource value: 0x7f050002
-			public const int lvPeers = 2131034114;
+			public const int tvHeader = 2131034114;
 			
 			static Id()
 			{
