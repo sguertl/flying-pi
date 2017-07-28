13b219f9301018d7d3cf82eef2ad791bf9f8eac0
sguertl <sebastian.guertl@gmail.com>
2017-07-28 08:55:12 +0200

    Add height controller button

--- Dronection/Android/WiFi/WiFiDronection/WiFiDronection/Resources/Resource.Designer.cs
+++ Dronection/Android/WiFi/WiFiDronection/WiFiDronection/Resources/Resource.Designer.cs
@@ -78,158 +78,161 @@ namespace WiFiDronection
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050005
-			public const int JoystickView = 2131034117;
+			// aapt resource value: 0x7f050006
+			public const int JoystickView = 2131034118;
+			
+			// aapt resource value: 0x7f05000d
+			public const int btBackToMain = 2131034125;
 			
 			// aapt resource value: 0x7f05000c
-			public const int btBackToMain = 2131034124;
+			public const int btStart = 2131034124;
 			
-			// aapt resource value: 0x7f05000b
-			public const int btStart = 2131034123;
+			// aapt resource value: 0x7f050020
+			public const int btnBackHelp = 2131034144;
 			
-			// aapt resource value: 0x7f05001f
-			public const int btnBackHelp = 2131034143;
-			
-			// aapt resource value: 0x7f050023
-			public const int btnBackLog = 2131034147;
-			
-			// aapt resource value: 0x7f05002e
-			public const int btnBackRawData = 2131034158;
-			
-			// aapt resource value: 0x7f050027
-			public const int btnConnect = 2131034151;
-			
-			// aapt resource value: 0x7f050029
-			public const int btnHelp = 2131034153;
-			
-			// aapt resource value: 0x7f050028
-			public const int btnShowLogs = 2131034152;
-			
-			// aapt resource value: 0x7f050031
-			public const int etDialogPassword = 2131034161;
-			
-			// aapt resource value: 0x7f05000a
-			public const int ivMode = 2131034122;
+			// aapt resource value: 0x7f050024
+			public const int btnBackLog = 2131034148;
 			
 			// aapt resource value: 0x7f05002f
-			public const int linechart = 2131034159;
+			public const int btnBackRawData = 2131034159;
 			
-			// aapt resource value: 0x7f050030
-			public const int lvData = 2131034160;
-			
-			// aapt resource value: 0x7f050021
-			public const int lvFiles = 2131034145;
-			
-			// aapt resource value: 0x7f05002c
-			public const int lvRawData = 2131034156;
-			
-			// aapt resource value: 0x7f050003
-			public const int rbPitchTrim = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int rbRollTrim = 2131034116;
-			
-			// aapt resource value: 0x7f050008
-			public const int rbThrottleLeft = 2131034120;
-			
-			// aapt resource value: 0x7f050009
-			public const int rbThrottleRight = 2131034121;
+			// aapt resource value: 0x7f050028
+			public const int btnConnect = 2131034152;
 			
 			// aapt resource value: 0x7f050002
-			public const int rbYawTrim = 2131034114;
+			public const int btnHeightControl = 2131034114;
 			
-			// aapt resource value: 0x7f050007
-			public const int rgControlMethod = 2131034119;
+			// aapt resource value: 0x7f05002a
+			public const int btnHelp = 2131034154;
+			
+			// aapt resource value: 0x7f050029
+			public const int btnShowLogs = 2131034153;
+			
+			// aapt resource value: 0x7f050032
+			public const int etDialogPassword = 2131034162;
+			
+			// aapt resource value: 0x7f05000b
+			public const int ivMode = 2131034123;
+			
+			// aapt resource value: 0x7f050030
+			public const int linechart = 2131034160;
+			
+			// aapt resource value: 0x7f050031
+			public const int lvData = 2131034161;
+			
+			// aapt resource value: 0x7f050022
+			public const int lvFiles = 2131034146;
+			
+			// aapt resource value: 0x7f05002d
+			public const int lvRawData = 2131034157;
+			
+			// aapt resource value: 0x7f050004
+			public const int rbPitchTrim = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int rbRollTrim = 2131034117;
+			
+			// aapt resource value: 0x7f050009
+			public const int rbThrottleLeft = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int rbThrottleRight = 2131034122;
+			
+			// aapt resource value: 0x7f050003
+			public const int rbYawTrim = 2131034115;
+			
+			// aapt resource value: 0x7f050008
+			public const int rgControlMethod = 2131034120;
 			
 			// aapt resource value: 0x7f050000
 			public const int sbTrimbar = 2131034112;
 			
-			// aapt resource value: 0x7f05001c
-			public const int tvAboutInfo = 2131034140;
-			
-			// aapt resource value: 0x7f05001a
-			public const int tvCredentials = 2131034138;
-			
-			// aapt resource value: 0x7f05002d
-			public const int tvDisplayRawData = 2131034157;
-			
-			// aapt resource value: 0x7f050022
-			public const int tvEmpty = 2131034146;
-			
-			// aapt resource value: 0x7f05002a
-			public const int tvFooter = 2131034154;
-			
-			// aapt resource value: 0x7f050024
-			public const int tvHeader = 2131034148;
-			
-			// aapt resource value: 0x7f050032
-			public const int tvHeaderDialog = 2131034162;
-			
-			// aapt resource value: 0x7f05000e
-			public const int tvHeaderHelp = 2131034126;
-			
-			// aapt resource value: 0x7f050020
-			public const int tvHeaderLog = 2131034144;
-			
-			// aapt resource value: 0x7f05002b
-			public const int tvHeaderRawData = 2131034155;
-			
-			// aapt resource value: 0x7f050006
-			public const int tvHeaderSettings = 2131034118;
-			
-			// aapt resource value: 0x7f050018
-			public const int tvHelpAbout = 2131034136;
-			
-			// aapt resource value: 0x7f050014
-			public const int tvHelpController = 2131034132;
-			
-			// aapt resource value: 0x7f050012
-			public const int tvHelpControllerSettings = 2131034130;
-			
-			// aapt resource value: 0x7f050016
-			public const int tvHelpLogFiles = 2131034134;
-			
-			// aapt resource value: 0x7f050010
-			public const int tvHelpStartScreen = 2131034128;
-			
-			// aapt resource value: 0x7f05000f
-			public const int tvHelpText = 2131034127;
-			
-			// aapt resource value: 0x7f050015
-			public const int tvHelpTextController = 2131034133;
-			
-			// aapt resource value: 0x7f050013
-			public const int tvHelpTextControllerSettings = 2131034131;
-			
-			// aapt resource value: 0x7f050017
-			public const int tvHelpTextLogFiles = 2131034135;
-			
-			// aapt resource value: 0x7f050011
-			public const int tvHelpTextStartScreen = 2131034129;
+			// aapt resource value: 0x7f05001d
+			public const int tvAboutInfo = 2131034141;
 			
 			// aapt resource value: 0x7f05001b
-			public const int tvLinkGitHub = 2131034139;
+			public const int tvCredentials = 2131034139;
 			
-			// aapt resource value: 0x7f05001d
-			public const int tvLinkHomepage = 2131034141;
+			// aapt resource value: 0x7f05002e
+			public const int tvDisplayRawData = 2131034158;
 			
-			// aapt resource value: 0x7f05000d
-			public const int tvListItem = 2131034125;
+			// aapt resource value: 0x7f050023
+			public const int tvEmpty = 2131034147;
+			
+			// aapt resource value: 0x7f05002b
+			public const int tvFooter = 2131034155;
+			
+			// aapt resource value: 0x7f050025
+			public const int tvHeader = 2131034149;
+			
+			// aapt resource value: 0x7f050033
+			public const int tvHeaderDialog = 2131034163;
+			
+			// aapt resource value: 0x7f05000f
+			public const int tvHeaderHelp = 2131034127;
+			
+			// aapt resource value: 0x7f050021
+			public const int tvHeaderLog = 2131034145;
+			
+			// aapt resource value: 0x7f05002c
+			public const int tvHeaderRawData = 2131034156;
+			
+			// aapt resource value: 0x7f050007
+			public const int tvHeaderSettings = 2131034119;
+			
+			// aapt resource value: 0x7f050019
+			public const int tvHelpAbout = 2131034137;
+			
+			// aapt resource value: 0x7f050015
+			public const int tvHelpController = 2131034133;
+			
+			// aapt resource value: 0x7f050013
+			public const int tvHelpControllerSettings = 2131034131;
+			
+			// aapt resource value: 0x7f050017
+			public const int tvHelpLogFiles = 2131034135;
+			
+			// aapt resource value: 0x7f050011
+			public const int tvHelpStartScreen = 2131034129;
+			
+			// aapt resource value: 0x7f050010
+			public const int tvHelpText = 2131034128;
+			
+			// aapt resource value: 0x7f050016
+			public const int tvHelpTextController = 2131034134;
+			
+			// aapt resource value: 0x7f050014
+			public const int tvHelpTextControllerSettings = 2131034132;
+			
+			// aapt resource value: 0x7f050018
+			public const int tvHelpTextLogFiles = 2131034136;
+			
+			// aapt resource value: 0x7f050012
+			public const int tvHelpTextStartScreen = 2131034130;
+			
+			// aapt resource value: 0x7f05001c
+			public const int tvLinkGitHub = 2131034140;
 			
 			// aapt resource value: 0x7f05001e
-			public const int tvThirdParty = 2131034142;
+			public const int tvLinkHomepage = 2131034142;
+			
+			// aapt resource value: 0x7f05000e
+			public const int tvListItem = 2131034126;
+			
+			// aapt resource value: 0x7f05001f
+			public const int tvThirdParty = 2131034143;
 			
 			// aapt resource value: 0x7f050001
 			public const int tvTrimValue = 2131034113;
 			
-			// aapt resource value: 0x7f050019
-			public const int tvVersion = 2131034137;
+			// aapt resource value: 0x7f05001a
+			public const int tvVersion = 2131034138;
+			
+			// aapt resource value: 0x7f050027
+			public const int tvWifiMac = 2131034151;
 			
 			// aapt resource value: 0x7f050026
-			public const int tvWifiMac = 2131034150;
-			
-			// aapt resource value: 0x7f050025
-			public const int tvWifiName = 2131034149;
+			public const int tvWifiName = 2131034150;
 			
 			static Id()
 			{
