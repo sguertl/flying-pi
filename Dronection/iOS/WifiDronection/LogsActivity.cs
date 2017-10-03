e1019e0d0c6e9061d8ae269d0a705a636675fbd5
chewes99 <chewes@gmx.at>
2017-10-03 14:17:26 +0200

    Fixed some Bugs

--- Dronection/iOS/WifiDronection/LogsActivity.cs
+++ Dronection/iOS/WifiDronection/LogsActivity.cs
@@ -4,12 +4,17 @@ using System.Drawing;
 using CoreGraphics;
 using Foundation;
 using UIKit;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace WiFiDronection
 {
     [Register("LogsActivity")]
     public class LogsActivity : UIView
     {
+
+        private static readonly string TAG = "LogsActivity";
+
         private String[] path;
         private NSString dataPath;
         private NSString documentsDirectory;
@@ -32,47 +37,102 @@ namespace WiFiDronection
 
         public void CreateFolder()
         {
-            NSString foldername = new NSString("MyFolder");
-
-            documentsDirectory = new NSString(path[0]);
-            dataPath = documentsDirectory.AppendPathComponent(foldername);
-
             try
             {
+                NSString foldername = new NSString("MyFolder");
+
+                documentsDirectory = new NSString(path[0]);
+                dataPath = documentsDirectory.AppendPathComponent(foldername);
+
+
                 NSFileManager.DefaultManager.CreateDirectory(dataPath, false, null);
             }catch(Exception ex)
             {
-               
+              
             }
         }
 
         public void ReadFile()
         {
-
-        }
-
-        public void WriteFile()
-        {
-            NSString folderPath = dataPath.AppendPathComponent(new NSString("testfile.txt"));
-
-
-            NSString someText = new NSString("Test");
-
-            NSData data = someText.Encode(NSStringEncoding.UTF8);
-
-            NSCoder ncs = new NSCoder();
-
-            
-
-
             try
             {
+                // Gets the direct path to the file
+                NSString folderPath = dataPath.AppendPathComponent(new NSString("testfile.txt"));
 
-            }catch(Exception ex)
+                // Handle the data and read it from the specific path
+                NSFileHandle nsfh = NSFileHandle.OpenRead(folderPath);
+
+                // Gets the data from the file
+                NSData data =  nsfh.ReadDataToEndOfFile();
+
+                string text = data.ToString();
+
+                
+
+            }
+            catch (Exception ex)
             {
+                // Failed read data from a file
 
             }
         }
 
+        public void WriteFile()
+        {
+          
+            try
+            {
+                // Gets the direct path to the file
+                NSString folderPath = dataPath.AppendPathComponent(new NSString("testfile.txt"));
+
+                // The text who should be written
+                NSString someText = new NSString("Test");
+
+                // The data
+                NSData data = someText.Encode(NSStringEncoding.UTF8);
+
+                // Handle the data and writes it to the specific path
+                NSFileHandle nsfh = NSFileHandle.OpenWrite(folderPath);
+
+                // Writes the data
+                nsfh.WriteData(data);
+
+
+            }
+            catch(Exception ex)
+            {
+                // Failed write data into a file
+
+            }
+        }
+
+
+        public void TestWrite()
+        {
+            // Get all directories
+            var directories = Directory.EnumerateDirectories("./");
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            // Reading files
+            var text = File.ReadAllText("TestData/ReadMe.txt");
+            Console.WriteLine(text);
+
+
+            // Creating Files
+            var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+           
+            var filename = Path.Combine(documents, "Write.txt");
+            File.WriteAllText(filename, "Write this text into a file");
+
+            // Creating a directory
+            var documents2 = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var directoryname = Path.Combine(documents, "NewDirectory");
+
+            
+
+        }
     }
 }
